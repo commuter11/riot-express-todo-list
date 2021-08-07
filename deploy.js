cac95ef97b7bd9e98c73371c831accdcb96b1948
Steven <sgalland@mail.com>
2021-08-07 09:59:30 -0700

    ip address update fail

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '100.26.50.27',
+      host: '52.87.211.246',
       username: 'ubuntu',
       privateKey: 'TODO-key.pem'
     })
